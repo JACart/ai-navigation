287f02631178a07923d79511ad8f61320cb9abc5
weilannw <weilannw@dukes.jmu.edu>
2018-01-27 15:14:03 -0500

    some basic ros nodes

--- catkin_ws/build/agc_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/weilannw/catkin_ws/src/agc_test
-# Build directory: /home/weilannw/catkin_ws/build/agc_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
