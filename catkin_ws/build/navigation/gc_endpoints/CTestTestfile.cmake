85bb34439739fd0953085bed305474fd5cc0da80
weilannw <weilannw@dukes.jmu.edu>
2018-02-15 21:07:14 -0500

    .

--- catkin_ws/build/navigation/gc_endpoints/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/weilannw/Autonomous-Navigation/catkin_ws/src/navigation/gc_endpoints
-# Build directory: /home/weilannw/Autonomous-Navigation/catkin_ws/build/navigation/gc_endpoints
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/navigation/gc_planning/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/weilannw/Autonomous-Navigation/catkin_ws/src/navigation/gc_planning
-# Build directory: /home/weilannw/Autonomous-Navigation/catkin_ws/build/navigation/gc_planning
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
