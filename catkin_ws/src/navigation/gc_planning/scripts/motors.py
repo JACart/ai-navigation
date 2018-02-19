23808463fd26e5787a45fe9f5d38e76c58eedb09
Allen <allenzm@l200206.cs.jmu.edu>
2018-02-19 11:04:56 -0500

    added code for a subsriber

--- catkin_ws/src/navigation/gc_planning/scripts/motors.py
+++ catkin_ws/src/navigation/gc_planning/scripts/motors.py
@@ -1,5 +1,6 @@
 import rospy
 import serial
+from navigation_msgs.msg import vel_angle
 
 speed_port = '/dev/ttyACM0'
 turn_port = ''
@@ -14,7 +15,7 @@ class MotorNode(object):
         rospy.init_node('motor_node')
         rospy.loginfo("Starting motor node!")
         #Connect to arduino for sending speed
-        speed_ser = serial.Serial(speed_port, 9600)
+        self.speed_ser = serial.Serial(speed_port, 9600)
         rospy.loginfo("Speed serial established")
         """
         #Connect to arduino for steering
@@ -22,18 +23,30 @@ class MotorNode(object):
         rospy.loginfo("Steering serial established")
         """
         self.speed_string = ''
-        current_speed = float(input("Input current speed:"))
-        goal_speed = float(input("Input desired speed:"))
-        while current_speed<goal_speed+test_increment:
-            self.speed_string =  str(goal_speed)+ ',' + str(current_speed)
+        response = "No response yet"
+        
+        #Code for testing
+        response = "No response yet"
+        self.current_speed = float(input("Input current speed:"))
+        self.goal_speed = float(input("Input desired speed:"))
+        while self.current_speed<self.goal_speed+test_increment:
+            self.speed_string =  str(self.goal_speed)+ ',' + str(self.current_speed)
             print "String being sent: "+self.speed_string
-            speed_ser.write(self.speed_string.encode())
-            response = speed_ser.readline()
+            self.speed_ser.write(self.speed_string.encode())
+            #response = self.speed_ser.readline()
             print "Response: "+response
-            current_speed = current_speed +test_increment
+            self.current_speed = self.current_speed +test_increment
             rospy.sleep(0.5)
+        rospy.loginfo("Testing complete")
+        #Code for using subscriber
+        self.motion_subscriber = rospy.Subscriber('/vel_angle', vel_angle, self.motion_callback, queue_size = 10)
         rospy.spin()
-
-if __name__ == "__main__":
-    
+        
+    def motion_callback(planned_vel_angle):
+        velocity = planned_vel_angle.vel
+        angle  = planned_vel_angle.angle
+        self.speed_string =  str(self.goal_speed)+ ',' + str(self.current_speed)
+        self.speed_ser.write(self.speed_string.encode())
+        rospy.loginfo("String being sent: "+self.speed_string)
+if __name__ == "__main__": 
     MotorNode()
