1d589207b268333c81a0463d200f1028eb792f58
weilannw <weilannw@dukes.jmu.edu>
2018-02-19 00:59:31 -0500

    added new types, node that converts to cmd_vel, still having problems running the api nodes

--- catkin_ws/src/navigation/gc_planning/scripts/control.py
+++ catkin_ws/src/navigation/gc_planning/scripts/control.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-
+#this will be replaced with PID control
 import rospy
 import math
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
-from std_msgs.msg import String
+from navigation_msgs.msg import vel_angle
+from navigation_msgs.msg import vel_angle_step
 input_angle = 0.0
 input_vel = 0.0
-vel = 0.0
-angle = 0.0
+cmd_msg = vel_angle()
+cmd_msg.vel = 0.0
+cmd_msg.vel = 0.0
 vel_step = 0.0
 angle_step = 0.0
 def control():
@@ -16,31 +18,28 @@ def control():
     global angle
     global vel_step
     global angle_step
-    cmd_vel_p = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    instr_s = rospy.Subscriber('plan_instr', String, get_input)
+    global cmd_msg
+    cmd_p = rospy.Publisher('nav_cmd', vel_angle, queue_size=10)
+    instr_s = rospy.Subscriber('nav_instr', vel_angle_step, get_input)
     rospy.init_node('control', anonymous=False)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-	vel = step_toward_value(vel, input_vel, vel_step)
-        angle = step_toward_value(angle, input_angle, angle_step)
-        twist_msg = Twist()
-	twist_msg.linear.x = vel
-	twist_msg.angular.z = angle
-        cmd_vel_p.publish(twist_msg)
-	rospy.loginfo("\n\tForward velocity instruction: "+str(float(vel))+
-		      "\n\tWheel angle instruction:      "+str(float(angle))+"\n")
+	cmd_msg.vel = step_toward_value(cmd_msg.vel, input_vel, vel_step)
+        cmd_msg.angle = step_toward_value(cmd_msg.angle, input_angle, angle_step)
+        cmd_p.publish(cmd_msg)
+	rospy.loginfo("\n\tForward velocity instruction: "+str(float(cmd_msg.vel))+
+		      "\n\tWheel angle instruction:      "+str(float(cmd_msg.angle))+"\n")
         rate.sleep()
 
-def get_input(value):
+def get_input(vel_angle_instr):
     global input_vel
     global input_angle
     global angle_step
     global vel_step
-    arr = value.data.split(' ')
-    input_vel = float(arr[0]) if arr.__len__() > 0 else 0.0
-    input_angle = float(arr[1]) if arr.__len__() > 1 else 0.0
-    vel_step = float(arr[2]) if arr.__len__() > 2 else 0.01
-    angle_step = float(arr[3]) if arr.__len__() > 3 else 10.0
+    input_vel = float(vel_angle_instr.vel)
+    input_angle = float(vel_angle_instr.angle)
+    vel_step = float(vel_angle_instr.vel_step)
+    angle_step = float(vel_angle_instr.angle_step)
 
 def step_toward_value(cur, dest, step): 
     diff = abs(dest - cur)
