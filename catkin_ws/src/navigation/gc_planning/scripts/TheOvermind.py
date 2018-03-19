d9da27dfa62d60a9ff0ec7add9d06bc119d875a9
weilannw <weilannw@dukes.jmu.edu>
2018-03-19 17:40:48 -0400

    getting closer

--- catkin_ws/src/navigation/gc_planning/scripts/TheOvermind.py
+++ catkin_ws/src/navigation/gc_planning/scripts/TheOvermind.py
@@ -2,41 +2,55 @@
 
 import rospy
 import math
-import control, point_to_goal
-import waypoint_hander.py
+import Control, PointToGoal, WaypointHandler
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import PointCloud2
-from navigation_msgs.msg import EmergencyStop, LatLongPoint, WaypointsArray 
+from navigation_msgs.msg import EmergencyStop, LatLongPoint, WaypointsArray, VelAngle 
 
 hertz = 10 #rospy speed
 class TheOvermind(object):
     
     def __init__(self):
+	self.current_goal = None #current goal from list of goals
         self.vel_curr = 0.0 #current velocity
+	self.kill = False
+	#util classes
+	self.waypoints = WaypointHandler.WaypointHandler(0)
 	#publishers                                    
-        self.cmd_vel_p = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.vel_angle_p = rospy.Publisher('/vel_angle', VelAngle, queue_size=10)
         #subscribers	
 	self.odom_s = rospy.Subscriber('/pose_and_speed', Odometry, self.odom_callback, queue_size=10)
 	self.point_cloud_s = rospy.Subscriber('/2d_point_cloud', PointCloud2, self.point_cloud_callback, queue_size=10) 
 	self.killswitch_s = rospy.Subscriber('/stop', EmergencyStop, self.killswitch_callback, queue_size=10) 
 	self.waypoints_s = rospy.Subscriber('/waypoints', WaypointsArray, self.waypoints_callback, queue_size=10) 
-        rospy.init_node('The Overmind', anonymous=False)
+        rospy.init_node('TheOvermind', anonymous=False)
 	self.control() 
- 
+
+    ''' this is the main event loop '''
     def control(self):
 	global hertz
         rate = rospy.Rate(hertz) # 10hz
         while not rospy.is_shutdown():
-       	    #if killswitch publish 0 to cmd_vel
+       	    #if killswitch publish 0 to vel_angle's vel_curr, and leave the angle the same
             rate.sleep()    
     
     def odom_callback(self, msg):
-        pass
+	self.odom_info = msg
+	#if the final goal has not been reached, get the next goal
+	if not self.waypoints.update_pos(self.odom_info, 0):
+	    self.current_goal = get_goal()
+	else:
+	    self.current_goal = None
+		
     def killswitch_callback(self, msg):
-	pass
+	if msg.emergency_stop:
+	    self.kill = True
+
     def waypoints_callback(self, msg):
-        pass
+	self.waypoints.set_points(msg.waypoints)
+	self.current_goal = get_goal()
+
     def point_cloud_callback(self, msg):
 	pass
 if __name__ == '__main__':
--- catkin_ws/src/navigation/gc_planning/scripts/WaypointHandler.py
+++ catkin_ws/src/navigation/gc_planning/scripts/WaypointHandler.py
@@ -2,7 +2,7 @@
 
 import rospy
 import math
-import point_to_goal
+import PointToGoal
 from navigation_msgs.msg import WaypointsArray, LatLongPoint 
 
 class WaypointHandler(object):
@@ -11,10 +11,11 @@ class WaypointHandler(object):
 	reached the target waypoint. '''
     def __init__(self, min_tolerance):
 	self.goal_index = None
-	self.waypoints = None
-	self.curr_pos = None
+	self.waypoints = None #waypoints are of type LatLongPoint
+	self.curr_pos = None 
 	self.min_tolerance = min_tolerance
 	self.tolerance = min_tolerance
+
     ''' current actual location updated from gps (pose_and_speed topic) ''' 
     def set_curr_location(self, odom_point, tolerance):
 	#the tolerance passed into this method must somehow be calculated from the Odometry
@@ -23,19 +24,21 @@ class WaypointHandler(object):
 	self.tolerance = min_tolerance if (min_tolerance > tolerance) else tolerance
 	    #tolerance is updated based on certainty of current location, minimum tolerance is
 	    #used if tolerance is too low
+
     ''' called when we are updated with a new list of points (the goal has been changed).
 	The old waypoints are forgotten/ignored completely. '''	
     def set_points(self, array):
 	self.waypoints = array
 	self.goal_index = 0
-    ''' increments the current point index and returns true if the final goal index has been reached.
+
+    ''' increments the current point index and returns true if there are no more indices.
 	should be called when the previous goal has been reached '''
     def next_point(self):
-	if(self.goal_index >= self.waypoints.length()-1)
-	    return true
-	else
+	if(self.goal_index >= self.waypoints.length()-1):
+	    return True
+	else:
 	    goal_index += 1
-	    return false
+	    return False
 	    
     ''' indicates whether the next point has been reached ''' 
     def reached_next_point(self):
@@ -43,17 +46,21 @@ class WaypointHandler(object):
 	return (math.abs
 		(point_to_goal.distance_between_points
 			(goal.latitude, goal.longitude, 
-			 self.curr_loc.pose.pose.x, self.curr_loc.pose.pose.y)) 
+			 self.curr_loc.pose.pose.position.x, self.curr_loc.pose.pose.position.y)) 
 		< tolerance)
+
     ''' current position is a message of type nav_msgs/Odometry. 
 	Returns true if the final goal point has been reached'''
     def update_pos(self, current_position, tolerance):
 	self.set_curr_location(current_position, tolerance)
-	return self.reached_next_point() && self.next_point()
+	return self.reached_next_point() and self.next_point()
 	#this line will call the necessary methods -- only returns
 	#true when the final goal has been reached	
+    
+    ''' retrieves the current goal '''
     def get_goal(self):
 	return self.waypoints[self.goal_index]	
+
 if __name__ == '__main__':
     try:
        WaypointHandler()
