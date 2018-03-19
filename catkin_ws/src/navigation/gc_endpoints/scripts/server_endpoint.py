d09c686537bf9dc82d3b07b57853afa05cdaba97
Domsky <domskyja@l200208.cs.jmu.edu>
2018-03-19 10:22:31 -0400

    Started server endpoint

---
+++ catkin_ws/src/navigation/gc_endpoints/scripts/server_endpoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python 
+import rospy
+import requests
+
+class server_endpoint(object):
+    def __init__(self):
+        rospy.init_node('server_endpoint')
+        #self.cmd_p = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+        #publish to waypoints topic
+
+        getGoal()
+
+        rospy.spin()
+
+def getGoal():
+    r = requests.get('https://practice-jad006.c9users.io/quotations')
+
+    print r.status_code
+    print r.ok
+
+    print r.headers['content-type']
+
+    #print r.text
+
+    #print r.json()
+
+
+    return r.ok
+
+	
+if __name__ == "__main__":
+    server_endpoint()
