4660964141c23b238e24eda1b943e80ef6a19977
JMU CS Student <student@l200203.cs.jmu.edu>
2018-03-26 16:58:58 -0400

    add_test_points now properly adds points from file to /waypoints for simulation

--- catkin_ws/src/navigation/gc_planning/scripts/the_overmind.py
+++ catkin_ws/src/navigation/gc_planning/scripts/the_overmind.py
@@ -13,76 +13,78 @@ tolerance = 1.0
 acceleration = 1.5 #acceleration in meters per second used by controller
 hertz = 10 #rospy execution cycles/s
 class TheOvermind(object):
-    
-    def __init__(self):
-	global hertz
-	global tolerance
 
-	self.odom = None
-	self.tolerance = tolerance #allowed imprecision for reaching a waypoint
-	self.current_goal = None #current goal from list of goals
-        self.vel_curr = 0.0 #current velocity
-	self.kill = False
-	self.vel_angle = VelAngle()
-	#util classes
-	self.waypoints = waypoint_handler.WaypointHandler(self.tolerance)
-	self.controller = controller.Controller(hertz)
-	#publishers                                    
-        self.vel_angle_p = rospy.Publisher('/vel_angle', VelAngle, queue_size=10)
-        #subscribers	
-	self.odom_s = rospy.Subscriber('/pose_and_speed', Odometry, self.odom_callback, queue_size=10)
-	self.point_cloud_s = rospy.Subscriber('/2d_point_cloud', PointCloud2, self.point_cloud_callback, queue_size=10) 
-	self.killswitch_s = rospy.Subscriber('/stop', EmergencyStop, self.killswitch_callback, queue_size=10) 
-	self.waypoints_s = rospy.Subscriber('/waypoints', WaypointsArray, self.waypoints_callback, queue_size=10) 
-        rospy.init_node('the_overmind', anonymous=False)
-	self.control() 
+    def __init__(self):
+	    global hertz
+	    global tolerance
+
+	    self.odom = None
+	    self.tolerance = tolerance #allowed imprecision for reaching a waypoint
+	    self.current_goal = None #current goal from list of goals
+            self.vel_curr = 0.0 #current velocity
+	    self.kill = False
+	    self.vel_angle = VelAngle()
+	    #util classes
+	    self.waypoints = waypoint_handler.WaypointHandler(self.tolerance)
+	    self.controller = controller.Controller(hertz)
+	    #publishers                                    
+            self.vel_angle_p = rospy.Publisher('/vel_angle', VelAngle, queue_size=10)
+            #subscribers	
+	    self.odom_s = rospy.Subscriber('/pose_and_speed', Odometry, self.odom_callback, queue_size=10)
+	    self.point_cloud_s = rospy.Subscriber('/2d_point_cloud', PointCloud2, self.point_cloud_callback, queue_size=10) 
+	    self.killswitch_s = rospy.Subscriber('/stop', EmergencyStop, self.killswitch_callback, queue_size=10) 
+	    self.waypoints_s = rospy.Subscriber('/waypoints', WaypointsArray, self.waypoints_callback, queue_size=10) 
+            rospy.init_node('the_overmind', anonymous=False)
+	    self.control() 
 
     ''' main event loop '''
     def control(self):
-	global hertz
-        rate = rospy.Rate(hertz) # 10hz
-        while not rospy.is_shutdown():
-       	    #if killswitch, publish 0 to vel_angle's vel_curr, and leave the angle the same	
-	    self.controller_handler()
-	    self.vel_angle_p.publish(self.vel_angle)
-	    rate.sleep()
-    
+	    global hertz
+            rate = rospy.Rate(hertz) # 10hz
+            while not rospy.is_shutdown():
+           	    #if killswitch, publish 0 to vel_angle's vel_curr, and leave the angle the same	
+	        self.controller_handler()
+	        self.vel_angle_p.publish(self.vel_angle)
+
+
+	        rate.sleep()
+        
     def odom_callback(self, msg):
-	self.odom = msg
-	self.vel_angle.vel_curr = msg.twist.twist.linear.x
-	#if the final goal has not been reached, get the next goal
-	if not self.waypoints.update_pos(msg, 0):
-	    self.current_goal = self.waypoints.get_goal()
-	else:
-	    self.current_goal = None
+	    self.odom = msg
+	    self.vel_angle.vel_curr = msg.twist.twist.linear.x
+	    #if the final goal has not been reached, get the next goal
+	    if not self.waypoints.update_pos(msg, 0):
+	        self.current_goal = self.waypoints.get_goal()
+	    else:
+	        self.current_goal = None
 
     ''' set a flag to true if emergency stop is activated '''		
     def killswitch_callback(self, msg):
-	if msg.emergency_stop:
-	    self.kill = True
+	    if msg.emergency_stop:
+	        self.kill = True
 
     def waypoints_callback(self, msg):
-	#replace the waypoints array with new data
-	self.waypoints.set_points(msg.waypoints)
-	self.current_goal = get_goal()
+	    #replace the waypoints array with new data
+	    self.waypoints.set_points(msg.waypoints)
+	    self.current_goal = get_goal()
 
     ''' handles controller related things in the main loop '''
     def controller_handler(self):
-	global acceleration
-	global max_speed
-	self.controller.step()
-	if self.current_goal and self.actual_speed == 0.0:
-	    self.controller.accelerate(acceleration, max_speed)
-	elif self.in_deceleration_range():
-	    self.controller.accelerate(acceleration, 0) 
-	self.vel_angle.vel = self.controller.get_velocity()
-	self.vel_angle.angle = self.controller.get_angle()
+	    global acceleration
+	    global max_speed
+	    self.controller.step()
+	    if self.current_goal and self.actual_speed == 0.0:
+	        self.controller.accelerate(acceleration, max_speed)
+	    elif self.in_deceleration_range():
+	        self.controller.accelerate(acceleration, 0) 
+	    self.vel_angle.vel = self.controller.get_velocity()
+	    self.vel_angle.angle = self.controller.get_angle()
    
     def is_approx(actual, expected, tolerance):
-	return expected - tolerance < actual < expected + tolerance
+	    return expected - tolerance < actual < expected + tolerance
  
     def point_cloud_callback(self, msg):
-	pass
+	    pass
 
     ''' Checks if in range to start decelerating (the range is based on tolerance). 
         The car is in range when, if it were to decelerate, the time it takes to reach 0 distance from the goal 
@@ -90,11 +92,11 @@ class TheOvermind(object):
 	the equation dist = speed^2/2*acceleration indicates this. A constant
 	acceleration is used to find this equivalence '''
     def in_deceleration_range(self):
-	global acceleration #hardcoded acceleration constant
-	if not (self.waypoints and self.waypoints.waypoints):
-	    return False
-	dist = waypoints.distance_from_goal()
-	return is_approx(dist, (self.speed * self.speed)/2*acceleration, self.tolerance)
+	    global acceleration #hardcoded acceleration constant
+	    if not (self.waypoints and self.waypoints.waypoints):
+	        return False
+	    dist = waypoints.distance_from_goal()
+	    return is_approx(dist, (self.speed * self.speed)/2*acceleration, self.tolerance)
   
 if __name__ == '__main__':
     try:
