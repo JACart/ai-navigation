666f6e52466121af39195724d8d6165764f2f75e
JMU CS Student <student@l200203.cs.jmu.edu>
2018-04-10 18:54:03 -0400

    Fixed more VelAngle

--- catkin_ws/src/navigation/gc_testing/gc_testing_description/scripts/vel_angle_to_twist.py
+++ catkin_ws/src/navigation/gc_testing/gc_testing_description/scripts/vel_angle_to_twist.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python 
 import rospy
-from navigation_msgs.msg import vel_angle
+from navigation_msgs.msg import VelAngle
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 class VelAngleToTwist(object):
     def __init__(self):
-      rospy.init_node('VelAngleToTwist')
-      self.cmd_p = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
-      self.vel_angle_s = rospy.Subscriber('vel_angle', vel_angle, self.convert, queue_size = 10)
-      self.twist_msg = Twist()
-      rospy.spin()
+        rospy.init_node('VelAngleToTwist')
+        self.cmd_p = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        self.vel_angle_s = rospy.Subscriber('/vel_angle', VelAngle, self.convert, queue_size = 10)
+        self.twist_msg = Twist()
+        rospy.spin()
     def convert(self, vel_angle):
-	self.twist_msg.linear.x = vel_angle.angle
-	self.twist_msg.linear.y = vel_angle.vel
-	self.cmd_p.publish(self.twist_msg)
+	    self.twist_msg.linear.x = vel_angle.angle
+	    self.twist_msg.linear.y = vel_angle.vel
+	    self.cmd_p.publish(self.twist_msg)
 	
 if __name__ == "__main__":
     VelAngleToTwist()
