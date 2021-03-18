439f15840292f38d93db3db5c99c21f3a1be6b0d
Nicole Stock <stockne@dukes.jmu.edu>
2021-03-18 19:08:05 -0400

    Adding option for vel=2.7 m/s to simulate velocity of cart normally

--- cart_simulator/teleop.py
+++ cart_simulator/teleop.py
@@ -34,6 +34,7 @@ class teleop(object):
         o = 111
         i = 105
         u = 117
+        l = 108
     
         angle_max = 70
         angle_step = 0.5
@@ -42,9 +43,10 @@ class teleop(object):
         stdscr.nodelay(True)
         rate = rospy.Rate(10) 
         stdscr.addstr(0,0,'Move with WASD, X for hard stop and Y for centering the wheel')
-        stdscr.addstr(1,0,'p = comfortable stop, o = obstacle in 2m, i = obstacle in 5m, u = obstacle in 15m')
-        stdscr.addstr(2,0,'CTRL-C to exit')
-        stdscr.addstr(3,0,'TURNING       WHEEL ANGLE')
+        stdscr.addstr(1,0,'p= comfortable stop, o= obstacle 2m, i= obstacle 5m, l = start at 2.7')
+        stdscr.addstr(2,0,'u= obstacle in 15m (wont stop, outside set safe range)')
+        stdscr.addstr(3,0,'CTRL-C to exit')
+        stdscr.addstr(4,0,'TURNING       WHEEL ANGLE')
         stdscr.addstr(6,0,'FORWARD MOVEMENT')
 
         while not rospy.is_shutdown():
@@ -90,11 +92,16 @@ class teleop(object):
                 self.msg.vel_curr = 1
                 self.msg.vel = -15
                 velstr = "obstacle in 15m - should not stop"
+                # collision_detector only stops for obstacles within 10 ft
+            elif keyval == l:
+                self.msg.vel_curr = 0.01
+                self.msg.vel = 2.7
+                velstr = "start moving at 2.7 m/s"
 
             self.prev_key = keyval
             self.motion_pub.publish(self.msg)
             outstr = anglestr +  velstr + str(self.msg.angle)
-            stdscr.addstr(4,0,anglestr + str(self.msg.angle))
+            stdscr.addstr(5,0,anglestr + str(self.msg.angle))
             stdscr.addstr(7,0,velstr)
             rate.sleep()
 
